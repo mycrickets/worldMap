4efdd34b4262fb598574db4a8f0ac9ee7b102c8b
CMRicketts <ricketc17@gmail.com>
2018-08-23 20:50:27 -0500

    from laptop

--- src/app/cookie-jar/cookie-jar.component.ts
+++ src/app/cookie-jar/cookie-jar.component.ts
@@ -9,25 +9,33 @@ export class CookieJarComponent implements OnInit {
 
 
   yesCookies(){
+    console.log("hit yes cookie");
     document.cookie = "returning=true;expires=Mon, 31 Dec 2018 12:00:00 UTC; path=/";
     this.closeModal()
   }
 
 
   noCookies() {
+    console.log("hit no cookie");
     this.closeModal()
   }
 
   closeModal(){
+    console.log("hit close modal");
     document.getElementById('modal-box').remove();
     if(!this.isReturning()){
+      console.log("hit activate splash");
       document.getElementById('splash-container').classList.add('is-active')
     }
   }
 
   isReturning(){
+    console.log("hit is returning");
     let returning = document.cookie;
-    return returning != ""
+    if ("returning" in returning.split("=")){
+      return true;
+    }
+    return false;
   }
 
   constructor() { }
